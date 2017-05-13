6e59f5b58555dda60dfdf50372390b91881ba8c2
Jack Liu <jacknlliu@gmail.com>
2017-05-13 20:34:24 +0800

    remove xterm condition in install_ursim script

--- universal-robot-simulator/scripts/install_ursim.sh
+++ universal-robot-simulator/scripts/install_ursim.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
-set -e
-
-export UR_DOWNLOAD_SITE="https://s3-eu-west-1.amazonaws.com/ur-support-site/28546/ursim-3.4.1-59.tar.gz"
-
-export URSIMVERSION="3.4.1-59"
-
-export INSTALL_DIR=$1
-export URSIM_USER=$2
-
+set -e \
+    && export UR_DOWNLOAD_SITE="https://s3-eu-west-1.amazonaws.com/ur-support-site/28546/ursim-3.4.1-59.tar.gz" \
+    && export URSIMVERSION="3.4.1-59"  \
+    && export INSTALL_DIR=$1  \
+    && export URSIM_USER=$2  \
+    && echo "INSTALL_DIR: $INSTALL_DIR \n URSIM_USER: $URSIM_USER" \
+    \
 # install ursim
-cd $INSTALL_DIR && wget -O ursim.tar.gz ${UR_DOWNLOAD_SITE} && tar -xvzf ursim.tar.gz \
-&& mv  ursim-${URSIMVERSION}  ursimpkg && cd ursimpkg \
-&& sed -i "s/pkexec bash -c/bash -c/g" ./install.sh \
-&& sed -i "s/apt-get -y install/aptitude -y -q -R/g" ./install.sh \
-&& ./install.sh \
-&& rm -f ../ursim.tar.gz \
-&& cd .. && chown -R $URSIM_USER:$URSIM_USER ursimpkg
+    && cd $INSTALL_DIR && wget -O ursim.tar.gz ${UR_DOWNLOAD_SITE} && tar -xvzf ursim.tar.gz && cd ursim-${URSIMVERSION} \
+    \
+# delete 36-37 lines for no xterm exit
+    && sed -i '36,37d' ./install.sh \
+    && sed -i 's/pkexec bash -c/bash -c/g' ./install.sh \
+    && sed -i 's/apt-get -y install/aptitude -y -q -R/g' ./install.sh \
+    && ./install.sh \
+    && cd .. && rm -f ./ursim.tar.gz \
+    && mv ursim-${URSIMVERSION} ursimpkg
+    && chown -R $URSIM_USER:$URSIM_USER ursimpkg
