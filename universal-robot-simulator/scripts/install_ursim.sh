f0bb87d6115b4c1dd03074817227f803ffb6f059
Jack Liu <jacknlliu@gmail.com>
2017-05-14 16:37:20 +0800

    add installation done tip for ursim shell script

--- universal-robot-simulator/scripts/install_ursim.sh
+++ universal-robot-simulator/scripts/install_ursim.sh
@@ -7,13 +7,14 @@ set -e \
     && echo "INSTALL_DIR: $INSTALL_DIR \n URSIM_USER: $URSIM_USER" \
     \
     && echo "install ursim" > /dev/null \
-    && cd $INSTALL_DIR && wget -O ursim.tar.gz ${UR_DOWNLOAD_SITE} && tar -xvzf ursim.tar.gz && cd ursim-${URSIMVERSION} \
+    && cd $INSTALL_DIR && wget -q -O ursim.tar.gz ${UR_DOWNLOAD_SITE} && tar -xvzf ursim.tar.gz && cd ursim-${URSIMVERSION} \
     \
     && echo "delete 36-37 lines for no xterm exit" > /dev/null \
     && sed -i '36,37c \\t echo \"just ignore xterm warning\" ' ./install.sh \
     && sed -i 's/pkexec bash -c/bash -c/g' ./install.sh \
-    && sed -i 's/apt-get -y install/aptitude install -y -q -R/g' ./install.sh \
+    && sed -i 's/apt-get -y install/apt-get -y install -q --no-install-recommends/g' ./install.sh \
     && sed -i '64,75d' ./install.sh  \
+    && echo "echo \" installation done! \""  >> ./install.sh \
     && ./install.sh \
     && cd .. && rm -f ./ursim.tar.gz \
     && mv ursim-${URSIMVERSION} ursimpkg \
