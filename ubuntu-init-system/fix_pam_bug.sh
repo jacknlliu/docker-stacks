d95d06d8155eea737cf151a3f0cbf67fac9261e6
Jack Liu <jacknlliu@gmail.com>
2017-05-18 21:56:44 +0800

    apt-get remove package not found in fix_pam_bug

--- ubuntu-init-system/fix_pam_bug.sh
+++ ubuntu-init-system/fix_pam_bug.sh
@@ -17,16 +17,16 @@ rm -rf *.deb *.gz *.dsc *.changes pam-*
 
 # Unfortunately there is no way to automatically remove build deps, so we do this manually.
 apt-get remove -y gdebi-core autoconf automake autopoint autotools-dev binutils bsdmainutils \
-	build-essential bzip2 cpp cpp-5 debhelper dh-autoreconf dh-strip-nondeterminism \
-	diffstat docbook-xml docbook-xsl dpkg-dev flex g++ g++-5 gcc gcc-5 gettext gettext-base \
-	groff-base intltool-debian libarchive-zip-perl libasan2 libasprintf0v5 libatomic1 \
-	libaudit-dev libc-dev-bin libc6-dev libcc1-0 libcilkrts5 libcrack2 libcrack2-dev libcroco3 \
-	libdb-dev libdb5.3-dev libdpkg-perl libfile-stripnondeterminism-perl libfl-dev libgc1c2 \
-	libgcc-5-dev libgdbm3 libgomp1 libgpm2 libicu55 libisl15 libitm1 liblsan0 libmpc3 \
-	libmpfr4 libmpx0 libpcre16-3 libpcre3-dev libpcre32-3 libpcrecpp0v5 libperl5.22 \
-	libpipeline1 libquadmath0 libselinux1-dev libsepol1-dev libsigsegv2 libstdc++-5-dev \
-	libtimedate-perl libtool libtsan0 libubsan0 libunistring0 libxml2 libxml2-utils \
-	libxslt1.1 linux-libc-dev m4 make man-db patch perl perl-modules-5.22 pkg-config \
+	build-essential bzip2 cpp cpp-4.* debhelper dh-autoreconf \
+	diffstat docbook-xml docbook-xsl dpkg-dev flex g++ g++-4.* gcc gcc-4.* gettext gettext-base \
+	groff-base intltool-debian libarchive-zip-perl libasan0 libasprintf0c2 libatomic1 \
+	libaudit-dev libc-dev-bin libc6-dev libcrack2 libcrack2-dev libcroco3 \
+	libdb-dev libdb5.3-dev libdpkg-perl libfl-dev libgc1c2 \
+	libgcc-4.*-dev libgdbm3 libgomp1 libgpm2 libicu52 libisl10 libitm1 libmpc3 \
+	libmpfr4 libpcre3 libpcre3-dev libpcrecpp0 libperl5.18 \
+	libpipeline1 libquadmath0 libselinux1-dev libsepol1-dev libsigsegv2 libstdc++-4.8-dev \
+	libtimedate-perl libtool libtsan0 libunistring0 libxml2 libxml2-utils \
+	libxslt1.1 linux-libc-dev m4 make man-db patch perl perl-modules pkg-config \
 	po-debconf quilt sgml-base sgml-data w3m xml-core xsltproc xz-utils
 
 apt-get remove -y gdebi-core
