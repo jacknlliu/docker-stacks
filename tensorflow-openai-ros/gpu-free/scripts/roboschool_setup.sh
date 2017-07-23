17332618928eb48a49112d7d75ff1ef5009f1477
Jack Liu <jacknlliu@gmail.com>
2017-07-23 18:46:04 +0800

    feat(tensorflow-openai-ros): add docker image tensorflow-openai-ros
    We add new docker image with tensorflow:latest-py3, openai/roboschool,
    and ros base version.
    
    jupyter-notebook-tensorflow-ros image add containerinit.sh in ros_entrypoint.sh.

---
+++ tensorflow-openai-ros/gpu-free/scripts/roboschool_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd /opt/ && git clone https://github.com/openai/roboschool.git
+cd /opt/ && git clone https://github.com/openai/rllab.git
+
+export ROBOSCHOOL_PATH=/opt/roboschool
+
+aptitude install -y -q -R cmake ffmpeg \
+    pkg-config qtbase5-dev libqt5opengl5-dev libassimp-dev libpython3.5-dev \
+    libboost-python-dev libtinyxml-dev
+
+cd /opt/ && git clone https://github.com/olegklimov/bullet3 -b roboschool_self_collision
+
+mkdir bullet3/build && cd bullet3/build
+cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 \
+      -DCMAKE_INSTALL_PREFIX:PATH=$ROBOSCHOOL_PATH/roboschool/cpp-household/bullet_local_install  \
+      -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  \
+      -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF \
+      -DBUILD_OPENGL3_DEMOS=OFF ..
+
+make -j4
+make install && cd /opt && pip3 install -e $ROBOSCHOOL_PATH
+
+chmod a+rwx -R /opt/roboschool /opt/rllab
--- tensorflow-openai-ros/gpu-free/scripts/ros_entrypoint.sh
+++ tensorflow-openai-ros/gpu-free/scripts/ros_entrypoint.sh
@@ -6,8 +6,7 @@ sudo /containerinit.sh
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 
-# start supervisor
-sudo /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+jupyter notebook -y --allow-root &
 
 # start CMD
 exec "$@"
