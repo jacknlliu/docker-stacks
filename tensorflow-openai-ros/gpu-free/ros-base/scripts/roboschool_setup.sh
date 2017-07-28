65e88c4ca3534fb979025f02fa2afce369b43846
Jack Liu <jacknlliu@gmail.com>
2017-07-28 13:22:31 +0800

    fix(tensorflow-openai-ros): fix pytorch installation

--- tensorflow-openai-ros/gpu-free/ros-base/scripts/roboschool_setup.sh
+++ tensorflow-openai-ros/gpu-free/ros-base/scripts/roboschool_setup.sh
@@ -2,6 +2,7 @@
 cd /opt/ && git clone https://github.com/openai/roboschool.git
 cd /opt/ && git clone https://github.com/openai/rllab.git
 
+# start install openai/roboschool
 export ROBOSCHOOL_PATH=/opt/roboschool
 
 aptitude install -y -q -R cmake ffmpeg \
@@ -20,4 +21,39 @@ cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 \
 make -j4
 make install && cd /opt && pip3 install --no-cache-dir -e $ROBOSCHOOL_PATH
 
-chmod a+rwx -R /opt/roboschool /opt/rllab
+chmod a+rwx -R /opt/roboschool
+
+# start install openai/rllab
+
+# ========== Special Deps ==========
+aptitude install -y -q -R git make cmake unzip
+pip3 install --no-cache-dir awscli
+# ALE requires zlib
+aptitude install -y -q -R zlib1g-dev
+# MUJOCO requires graphics stuff (Why?)
+apt-get -y build-dep glfw
+aptitude install -y -q -R libxrandr2 libxinerama-dev libxi6 libxcursor-dev
+# copied from requirements.txt
+#  pip install imageio tabulate nose
+aptitude install -y -q -R vim ack-grep
+pip3 install --upgrade pip
+# usual pip install pygame will fail
+apt-get build-dep -y python-pygame
+pip3 install --no-cache-dir Pillow
+
+# ========== OpenAI Gym ==========
+aptitude install -y -q -R libgtk2.0-0
+pip3 install gym
+#  apt-get -y install ffmpeg
+aptitude install -y -q -R libav-tools
+# set alias in /home/ros/.bashrc
+# alias ffmpeg="avconv"
+
+# install rllab dependencies
+pip3 install --no-cache-dir -r /requirements.txt
+
+# fix pytorch not spport for this python
+pip3 install http://download.pytorch.org/whl/cu80/torch-0.1.12.post2-cp35-cp35m-linux_x86_64.whl
+pip3 install --no-cache-dir torchvision
+
+chmod a+rwx -R  /opt/rllab
