07390a988085877a533a34fd04c271726f415b2d
Jack Liu <jacknlliu@gmail.com>
2017-08-15 01:31:09 +0800

    fix(tensorflow-openai-ros): fix echo PYTHONPATH to .bashrc error

--- tensorflow-openai-ros/gpu/ros-base/scripts/ros_entrypoint.sh
+++ tensorflow-openai-ros/gpu/ros-base/scripts/ros_entrypoint.sh
@@ -6,6 +6,8 @@ sudo /opt/scripts/container/containerinit.sh
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 
+export PYTHONPATH=$PYTHONPATH:/opt/tensorflow-models:/opt/tensorflow-models/slim
+
 jupyter notebook -y --allow-root &
 
 # start CMD
