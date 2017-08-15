4155935519e6c66ac635513c44e8bc685d08fb56
Jack Liu <jacknlliu@gmail.com>
2017-08-15 10:37:10 +0800

    refactor(tensorflow-openai-ros): use python site path file replacing PYTHONPATH config for ros and tensorflow-models

--- tensorflow-openai-ros/gpu-free/ros-base/scripts/ros_entrypoint.sh
+++ tensorflow-openai-ros/gpu-free/ros-base/scripts/ros_entrypoint.sh
@@ -3,11 +3,6 @@ set -e
 
 sudo /opt/scripts/container/containerinit.sh
 
-# setup ros environment
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-
-export PYTHONPATH=$PYTHONPATH:/opt/tensorflow-models:/opt/tensorflow-models/slim
-
 jupyter notebook -y --allow-root &
 
 # start CMD
