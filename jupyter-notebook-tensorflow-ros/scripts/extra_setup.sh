836fae66fd352b55882fa58ee0cb5cf156e80dee
Jack Liu <jacknlliu@gmail.com>
2017-09-12 13:54:40 +0800

    feat(jupyter-notebook): add keras package

--- jupyter-notebook-tensorflow-ros/scripts/extra_setup.sh
+++ jupyter-notebook-tensorflow-ros/scripts/extra_setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+JUPYTER_USER_NAME=jovyan
+
 # update sympy to fix some bugs
 conda update -n root -y sympy
 
@@ -12,18 +14,18 @@ source /opt/conda/bin/activate root
 python setup.py sdist && pip install dist/*.tar.gz
 
 # install ipyparallel
-pip install ipyparallel
+pip install ipyparallel keras
 
 source /opt/conda/bin/deactivate
 
 
 # set jupyter enable ipyparallel, refer to https://github.com/ipython/ipyparallel/issues/170
-su - jovyan
+su - $JUPYTER_USER_NAME
 source /opt/conda/bin/activate root
 
-jupyter serverextension enable --py ipyparallel --user
-jupyter nbextension install --py ipyparallel --user
-jupyter nbextension enable --py ipyparallel --user
+jupyter serverextension enable --py ipyparallel --user $JUPYTER_USER_NAME
+jupyter nbextension install --py ipyparallel --user $JUPYTER_USER_NAME
+jupyter nbextension enable --py ipyparallel --user $JUPYTER_USER_NAME
 
 source /opt/conda/bin/deactivate
 
